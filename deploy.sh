9bb83fb42d996e2886a62bab13b1c9780ec461c8
hotdog <hotdog929@gmail.com>
2017-07-31 17:30:37 +0800

    init

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"event-resolve\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\"}" 'http://test.ehanlin.com.tw/event/api/Deploy'
