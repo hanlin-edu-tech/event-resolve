80deda5946129ddf644f4c5156e757b7079fd00b
AsoTiesto <cute17852337569@gmail.com>
2018-06-26 17:11:02 +0800

    調整功能：使用 gulp upload gcp 解決上傳空白檔案的問題

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,18 @@
 const Q = require('q')
 const del = require('del')
 const gulp = require('gulp')
+const cache = require('gulp-cache')
+const imageMin = require('gulp-imagemin')
 const util = require('gulp-template-util')
 const gcPub = require('gulp-gcloud-publish')
-const Storage = require('@google-cloud/storage')
+const pngquant = require('imagemin-pngquant')
+const templateUtil = require('gulp-template-util')
 
-let bucketName = 'tutor-events'
+let bucketNameForTest = 'tutor-events-test'
+let bucketNameForProd = 'tutor-events'
 let projectId = 'tutor-204108'
-let keyFilename = './tutor.json'
-let projectName = 'resolve'
-
-const storage = new Storage({
-  projectId: projectId,
-  keyFilename: keyFilename
-})
+let keyFilename = 'tutor.json'
+let projectName = 'event/resolve/'
 
 let copyStaticTask = dest => {
   return () => {
@@ -31,51 +30,62 @@ let cleanTask = () => {
   return del(['dist', ''])
 }
 
-let removeEmptyFiles = () => {
-  let array = ['img', 'css', 'lib']
-  array.forEach(emptyFiles => {
-    storage
-      .bucket(bucketName)
-      .file(`/event/${projectName}/${emptyFiles}`)
-      .delete()
-      .then(() => {
-        console.log(`gs://${bucketName}/${emptyFiles} deleted.`)
-      })
-      .catch(err => {
-        console.error('ERROR:', err)
-      })
-  })
+let minifyImage = sourceImage => {
+  return gulp
+    .src(sourceImage, {
+      base: './src'
+    })
+    .pipe(cache(imageMin({
+      use: [pngquant({
+        speed: 7
+      })]
+    })))
+    .pipe(gulp.dest('./dist'))
 }
 
-gulp.task('uploadGcp', () => {
-  return gulp.src(['dist/**/*'])
+let uploadGCS = bucketName => {
+  return gulp
+    .src([
+      './dist/*.html',
+      './dist/css/**/*.css',
+      './dist/js/**/*.js',
+      './dist/lib/**/*.@(js|json|css|html)',
+      './dist/img/**/*.@(png|jpg|svg|gif)'
+    ], {
+      base: `${__dirname}/dist/`
+    })
     .pipe(gcPub({
       bucket: bucketName,
       keyFilename: keyFilename,
+      base: projectName,
       projectId: projectId,
-      base: `/event/${projectName}`,
       public: true,
-      transformDestination: path => {
-        return path
-      },
       metadata: {
-        cacheControl: 'max-age=315360000, no-transform, public'
+        cacheControl: 'private, no-transform'
       }
     }))
-})
+}
 
-gulp.task('removeEmptyFiles', () => {
-  removeEmptyFiles()
-})
+/* upload to gcp test */
+gulp.task('uploadGcpTest', uploadGCS.bind(uploadGCS, bucketNameForTest))
+
+/* upload to gcp test */
+gulp.task('uploadGcpProd', uploadGCS.bind(uploadGCS, bucketNameForProd))
 
 gulp.task('package', () => {
   let deferred = Q.defer()
   Q.fcall(() => {
     return util.logPromise(cleanTask)
-  }).then(() => {
-    return Q.all([
-      util.logStream(copyStaticTask('dist'))
-    ])
   })
+    .then(() => {
+      return Q.all([
+        templateUtil.logStream(minifyImage.bind(minifyImage, './src/img/**/*.png'))
+      ])
+    })
+    .then(() => {
+      return Q.all([
+        util.logStream(copyStaticTask('dist'))
+      ])
+    })
   return deferred.promise
 })
